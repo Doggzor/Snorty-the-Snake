078a6fcac9db496360c92cc5691a5cc4102bddbc
Dogg <Dogg@users.noreply.github.com>
2020-04-24 15:39:13 +0200

    Added Vec2 class for vectors

---
+++ Engine/Vec2.cpp
@@ -0,0 +1,61 @@
+#include "Vec2.h"
+#include <cmath>
+
+Vec2::Vec2(float in_x, float in_y)
+	:
+	x(in_x),
+	y(in_y)
+{
+}
+
+Vec2 Vec2::operator+(const Vec2& rhs) const
+{
+	return Vec2(x + rhs.x, y + rhs.y);
+}
+
+Vec2& Vec2::operator+=(const Vec2& rhs)
+{
+	return *this = *this + rhs;
+}
+
+Vec2 Vec2::operator*(float rhs) const
+{
+	return Vec2(x * rhs, y * rhs);
+}
+
+Vec2& Vec2::operator*=(float rhs)
+{
+	return *this = *this * rhs;
+}
+
+Vec2 Vec2::operator-(const Vec2& rhs) const
+{
+	return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2& Vec2::operator-=(const Vec2& rhs)
+{
+	return *this = *this - rhs;
+}
+
+float Vec2::GetLength() const
+{
+	return sqrt(GetLengthSq());
+}
+
+float Vec2::GetLengthSq() const
+{
+	return x * x + y * y;
+}
+
+Vec2& Vec2::Normalize()
+{
+	return *this = GetNormalized();
+}
+
+Vec2 Vec2::GetNormalized() const
+{
+	const float length = GetLength();
+	if (length != 0.0f) return *this * (1.0f / length);
+	else return *this;
+}
